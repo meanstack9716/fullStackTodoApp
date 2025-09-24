8acd2cbb8f76bd120a61a4573827fa1ce9be67c5
kaushaki kumari <kaushakikuamri@innotechteam.in>
2025-09-24 17:29:33 +0530

    fixed PR cmnt

--- frontend/src/interface/AddEditToDoModalProps.ts
+++ frontend/src/interface/AddEditToDoModalProps.ts
@@ -3,6 +3,6 @@ import { Todo } from "@/types/todo";
 export default interface AddEditTodoModalProps {
     isOpen: boolean;
     onClose: () => void;
-    onSave: (task: Todo) => void;
+    onSave?: (task: Todo) => void;
     task?: Todo | null;
 }
