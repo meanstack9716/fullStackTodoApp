7769fd03a7c1fc2fa4af6e53a25f74eb51206029
kaushaki kumari <kaushakikuamri@innotechteam.in>
2025-10-03 16:59:24 +0530

    fixed notification show

--- backend/cronJobs/startReminder.js
+++ backend/cronJobs/startReminder.js
@@ -10,14 +10,21 @@ const chunk = (arr, size) => {
     return out;
 };
 
+const stripHtmlTags = (str) => {
+    if (!str) return '';
+    return str.replace(/<[^>]*>/g, '');
+};
+
 const startReminderCron = () => {
-    cron.schedule('0 */2 * * *', async () => {
+    cron.schedule('*/2 * * * *', async () => {
+        // cron.schedule('0 */2 * * *', async () => {
         try {
             const now = new Date();
             const sixHoursLater = new Date(now.getTime() + 6 * 60 * 60 * 1000);
 
             const todos = await Todo.find({
                 expireAt: { $gte: now, $lte: sixHoursLater },
+
                 status: { $ne: TodoStatus.Completed },
             });
             if (!todos.length) return;
@@ -26,10 +33,25 @@ const startReminderCron = () => {
                 const subs = await PushSubscription.find({ userId: todo.user });
                 const tokens = subs.map(s => s.fcmToken).filter(Boolean);
                 if (!tokens.length) continue;
+
+                const now = new Date();
+                const expireTime = new Date(todo.expireAt);
+                const diffMs = expireTime - now;
+
+                const diffHours = Math.floor(diffMs / (1000 * 60 * 60));
+                const diffMinutes = Math.floor((diffMs % (1000 * 60 * 60)) / (1000 * 60));
+                let timeLeftText = '';
+                if (diffHours > 0) timeLeftText += `${diffHours} hour${diffHours > 1 ? 's' : ''}`;
+                if (diffMinutes > 0) {
+                    if (diffHours > 0) timeLeftText += ' ';
+                    timeLeftText += `${diffMinutes} minute${diffMinutes > 1 ? 's' : ''}`;
+                }
+
+                const descriptionNotification = stripHtmlTags(todo.description);
                 const notification = {
                     notification: {
-                        title: '📝 Task Reminder!',
-                        body: `${todo.title} is expiring in 6 hours!`,
+                        title: `📝 ${todo.title}`,
+                        body: `${descriptionNotification} is expiring in ${timeLeftText} !`,
                     },
                     webpush: {
                         headers: { TTL: `${6 * 60 * 60}` },
